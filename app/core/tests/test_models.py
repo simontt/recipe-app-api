ab9bca7d61af1fd09bd45245fc2ed7e5dd0d0406
Simon Tavernier <SimonT.Tavernier@UGent.be>
2020-06-05 18:51:31 +0200

    Flake8 shizzle.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
 
+
 def sample_user(email='test@londonappdev.com', password='testpass'):
     """Create a sample user for model testing."""
     return get_user_model().objects.create_user(email, password)
