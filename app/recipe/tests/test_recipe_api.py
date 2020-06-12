e5e676ed0cfbdca74b19c717c3342bc32c2146de
Simon Tavernier <SimonT.Tavernier@UGent.be>
2020-06-12 17:18:03 +0200

    Added update recipe tests.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -167,3 +167,48 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(ingredients.count(), 2)
         self.assertIn(ingredient_1, ingredients)
         self.assertIn(ingredient_2, ingredients)
+
+    def test_partial_update_recipe(self):
+        """Test updating a recipe with patch."""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user, name='Curry')
+
+        # Payload for recipe with a new tag and new title
+        payload = {'title': 'Chicken tikka', 'tags': [new_tag.id]}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        # Refreshed details in db to commit changes
+        recipe.refresh_from_db()
+
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_full_update_recipe(self):
+        """Test updating a recipe with put."""
+        # Put fully replaces object, also if not all fields in new payload
+        # (i.e. fields not in payload are removed)
+
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+
+        payload = {'title': 'Spaghetti carbonara',
+                   'time_minutes': 25,
+                   'price': 5.}
+
+        url = detail_url(recipe.id)
+
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+
+        # Tag should be removed
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
